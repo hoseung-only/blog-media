bf1bf49af99ca12987f1507c1f2aa4514554443d
HoseungJang <hsjang.dev@gmail.com>
2021-07-28 05:47:17 +0900

    add testcase for image router

---
+++ src/__test__/image.spec.ts
@@ -0,0 +1,24 @@
+import * as request from "supertest";
+import { expect } from "chai";
+
+import { app } from "../app";
+
+describe("Image Routers", () => {
+  describe("GET /presigned_post: get presinged post", () => {
+    context("When user requests", () => {
+      it("should return presigned post", async () => {
+        return request(app)
+          .get("/image/presigned_post")
+          .query({ fileName: "image", fileType: "png" })
+          .expect(200)
+          .then((response) => {
+            expect(response.body).has.property("url");
+            expect(response.body).has.property("fields");
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+    });
+  });
+});
