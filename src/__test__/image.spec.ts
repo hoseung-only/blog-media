8ebd973110173d6cd674ef4277d13c1b9d69939a
HoseungJang <hsjang.dev@gmail.com>
2021-08-01 23:09:46 +0900

    fix testcase

--- src/__test__/image.spec.ts
+++ src/__test__/image.spec.ts
@@ -3,17 +3,18 @@ import { expect } from "chai";
 
 import { app } from "../app";
 
-describe("Image Routers", () => {
-  describe("GET /presigned_post: get presinged post", () => {
+describe("Post Image Routers", () => {
+  describe("GET /:id/images/presigned_post: get presinged post", () => {
     context("When user requests", () => {
       it("should return presigned post", async () => {
         return request(app)
-          .get("/images/presigned_post")
+          .get("/posts/asdf/images/presigned_post")
           .query({ fileName: "image", fileType: "png" })
           .expect(200)
           .then((response) => {
             expect(response.body).has.property("url");
             expect(response.body).has.property("fields");
+            expect(response.body.fields.key).to.be.eq("/posts/asdf/image.png");
           })
           .catch((error) => {
             throw error;
